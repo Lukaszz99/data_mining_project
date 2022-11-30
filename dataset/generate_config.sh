00723a8b510f9874cf67e9f3f9e037c19a6a343e
Lukaszz99 <l.saw99@outlook.com>
2022-11-30 23:00:24 +0100

    Add .gitignore and acces_generator

---
+++ dataset/generate_config.sh
@@ -0,0 +1,16 @@
+echo "Type API Key"
+read -r api_key
+
+echo "Type API Key Secret"
+read -r secret_key
+
+echo "Type Access Token"
+read -r token
+
+echo "Type Access Token Secret"
+read -r secret_token
+
+echo "api_key: $api_key
+api_key_secret: $secret_key
+access_key: $token
+access_key_secret: $secret_token" >> access_config.yaml
